c407cc572c79d310140f3789bced13abc98cedef
Dianparir <dianparir1812@gmail.com>
2023-04-05 23:42:43 +0800

    posttest 6

--- posttest6_064_krisdayanti/lib/main.dart
+++ posttest6_064_krisdayanti/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:posttest6_064_krisdayanti/splash_screen.dart';
 
 //import file dart
+import 'splash_screen.dart';
 import 'landing_page.dart';
 import 'home_page.dart';
 import 'boyband.dart';
